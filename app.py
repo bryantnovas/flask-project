6fe2746d376f2ad72ac59b7ce24a746ab7585f68
Bryant Novas <bryant.novas@gmail.com>
2020-11-20 23:37:46 -0500

    Type: Feat added contact me form and created corresponding view & route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,16 @@ def jokes():
   resp = r.json()
   joke = resp['value']
   return render_template('jokes.html', joke=joke)
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+  if request.method == 'POST':
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    return render_template('form.html', name=name, email=email,message=message)
+  else:
+    return render_template('form.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
